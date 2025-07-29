5900d9fa9a695fbfa328b41c5b35d7013f6b4ef2
FrankDeveloper98 <frankdeveloper8@outlook.com>
2025-07-29 02:17:01 -0500

    fix import in usercontroller

--- app/Http/Controllers/User/UserMovieController.php
+++ app/Http/Controllers/User/UserMovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movies;
 use App\Models\UserFavorite;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
